c92a8461ac534f7025c7c2fd3da124fe189a009f
Artem Martynovich <artem.martynovich@gmail.com>
2020-06-01 09:31:25 +0600

    Frontend: stretch link in Restaurant card, add address and date.

--- frontend/restaurants/src/App.js
+++ frontend/restaurants/src/App.js
@@ -3,27 +3,40 @@ import 'bootstrap/dist/js/bootstrap.js';
 
 import React, {createRef, useEffect, useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar, faSignOutAlt, faUser, faUserTie, faUserCog, faStarHalf } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faSignOutAlt, faUser, faUserTie, faUserCog, faStarHalf,
+  faMapMarker, faMapMarkerAlt, faHistory } from '@fortawesome/free-solid-svg-icons'
 
 import './App.css';
 
 function Card() {
   return (
-      <div className="card mb-3 restaurants-card">
-        <div className="row no-gutters">
+      <div className="card mb-3 shadow-sm restaurants-card">
+        <div className="row">
           <div className="col-md-4">
-            <img src="https://via.placeholder.com/150x100" className="card-img" alt="..." />
+            <img src="https://via.placeholder.com/150x70" className="card-img" alt="..." />
           </div>
           <div className="col-md">
             <div className="card-body">
-              <h5 className="card-title d-inline-block ml-5">Card title</h5>
+              <h5 className="card-title d-inline-block">
+                <a href="#" className="stretched-link">Restaurant Name</a>
+              </h5>
               <span className="restaurants-stars-span d-inline-block float-right flex-nowrap">
                 <Stars staticRating={4.1}/>
                 <small className="text-muted align-text-top pl-2">4.1</small>
               </span>
               <p className="card-text text-justify">This is a wider card with supporting text below as a natural lead-in to
                 additional content. This content is a little bit longer.</p>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              <hr className="mt-0 mb-1"/>
+              <p className="card-text">
+                <small className="text-muted">
+                  <FontAwesomeIcon icon={faMapMarkerAlt} color="red" className="mr-2"/>
+                  15 Šetalište Kapetana Iva Vizina, Tivat
+                </small>
+                <small className="text-muted float-right pt-1">
+                  <FontAwesomeIcon icon={faHistory} className="mr-2" />
+                  {new Date().toLocaleString()}
+                </small>
+              </p>
             </div>
           </div>
         </div>
@@ -100,9 +113,17 @@ function RatingFilter() {
   return (
       <span className="nav-link" onMouseEnter={onMouseOver} onMouseLeave={onMouseOut} ref={spanRef}>
         <a className="nav-link d-inline" href="#">Rating: </a>
-        {!expanded && <a className={`text-primary ${ratingHover? "d-none": "d-inline"}`} href="#" tabIndex="-1"><u>All</u></a>}
-        {(ratingHover || expanded) && <Stars initialRating={2} onSelect={onStarSelect}/>}
-        {expanded && <a className="nav-link d-inline text-primary text-sm-center" href="#" tabIndex="-1" onClick={onStarReset}><small>Reset</small></a>}
+        {!expanded &&
+          <a className={`text-light ${ratingHover? "d-none": "d-inline"}`} href="#" tabIndex="-1"><u>All</u></a>
+        }
+        {(ratingHover || expanded) &&
+          <Stars initialRating={2} onSelect={onStarSelect}/>
+        }
+        {expanded &&
+          <a className="nav-link d-inline text-light text-sm-center" href="#" tabIndex="-1" onClick={onStarReset}>
+            <u><small>Reset</small></u>
+          </a>
+        }
       </span>
   )
 }
@@ -115,7 +136,7 @@ function Navbar() {
   };
 
   return(
-      <nav className="navbar navbar-expand-sm restaurants-card sticky-top navbar-light bg-light">
+      <nav className="navbar rounded navbar-expand-sm restaurants-card sticky-top navbar-dark bg-secondary shadow">
         <a className="navbar-brand" href="#">Restaurants</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -124,19 +145,22 @@ function Navbar() {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <div className="container">
-          <ul className="navbar-nav mr-auto">
+          <ul className="navbar-nav">
+            {/*<li className="nav-item flex-nowrap">*/}
+            {/*  <a className="nav-link">Back</a>*/}
+            {/*</li>*/}
             <li className="nav-item flex-nowrap">
               <RatingFilter/>
             </li>
           </ul>
           </div>
           <div className="btn-group" role="group">
-            <button type="button" className="btn btn-outline-primary text-nowrap restaurants-login-btn"
+            <button type="button" className="btn btn-sm btn-outline-light text-nowrap restaurants-login-btn"
                     data-toggle="tooltip" data-placement="bottom" title="Visitor" tabIndex="-1" onClick={ignoreClick}>
               <FontAwesomeIcon icon={faUser} className="mr-3"/>
               Artem Martynovich
             </button>
-            <button type="button" className="btn btn-outline-primary text-nowrap"
+            <button type="button" className="btn btn-sm btn-outline-light text-nowrap"
                     data-toggle="tooltip" data-placement="bottom" title="Log Out">
               <FontAwesomeIcon icon={faSignOutAlt} />
             </button>
