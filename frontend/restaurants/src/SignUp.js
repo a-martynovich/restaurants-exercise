7c817e38a8fe49b9358a56f726a1b68278e14d76
Artem Martynovich <artem.martynovich@gmail.com>
2020-06-10 13:15:56 +0600

    Frontend: implement SignUp.

--- frontend/restaurants/src/SignUp.js
+++ frontend/restaurants/src/SignUp.js
@@ -1,17 +1,54 @@
 import React, {createRef, useEffect, useState} from 'react';
-import {Row, Col, Container, Card, Button, InputGroup, Form} from 'react-bootstrap'
+import {Row, Col, Container, Card, Button, InputGroup, Form, Alert} from 'react-bootstrap'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAt, faKey} from "@fortawesome/free-solid-svg-icons";
+import {useMutation} from "react-query";
+import {fetchJSON} from "./Fetch";
 
 
-function SignUpDialog({onBack, onSignupClick}) {
+function SignUpDialog({onBack, onSignup}) {
+  const [mutate, {status, data, error, reset}] = useMutation(fetchJSON, {
+    onSuccess: async (data) => {
+      console.log('SIGNUP SUCCESS');
+      onSignup();
+    }
+  });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    let data = Object.fromEntries(new FormData(e.target));
+    data.username = data.email;
+    await mutate({
+      method: 'POST',
+      url: 'http://localhost:8000/signup/',
+      body: data
+    });
+  };
+
+  console.log(status, data, error);
+  let validation = {}, generic_error;
+  if(error) {
+    if(error.status==400 && error.body) {
+      if(error.body.errors) {
+        validation = error.body.errors;
+        if (validation.username)
+          validation.email = validation.username;
+      } else {
+        generic_error = error.body.non_field_errors && error.body.non_field_errors[0];
+      }
+    } else {
+      generic_error = error.text || error.message;
+    }
+  }
+
   return (
     <Col className="col-lg-5 col-md-8 align-self-end">
       <Card className="bg-light align-middle shadow rounded-lg">
         <Card.Body>
           <h4 className="card-title text-center">Sign Up</h4>
           <hr/>
-          <form method="post" >
+          <form id="myForm" name="myForm" onSubmit={onSubmit} >
             <Form.Row className="mb-3">
               <Col md={3} lg={3} sm={3} className="text-nowrap">
                 <Form.Label column lg={2}>
@@ -19,56 +56,69 @@ function SignUpDialog({onBack, onSignupClick}) {
                 </Form.Label>
               </Col>
               <Col>
-                <Form.Control as="select">
+                <Form.Control as="select" isInvalid={null}>
                   <option>Visitor</option>
                   <option>Owner</option>
                   <option>Admin</option>
                 </Form.Control>
+                <Form.Control.Feedback>...</Form.Control.Feedback>
               </Col>
             </Form.Row>
             <Form.Row className="mb-3">
               <Col className="p-0 pr-2">
-                <Form.Control placeholder="First Name" required/>
+                <Form.Control placeholder="First Name" required name="first_name"
+                              isInvalid={validation.first_name} onChange={reset}/>
+                <Form.Control.Feedback type="invalid">{validation.first_name && validation.first_name[0]}</Form.Control.Feedback>
               </Col>
               <Col className="p-0">
-                <Form.Control placeholder="Last Name" required/>
+                <Form.Control placeholder="Last Name" required name="last_name"
+                              isInvalid={validation.last_name}/>
+                <Form.Control.Feedback type="invalid">{validation.last_name && validation.last_name[0]}</Form.Control.Feedback>
               </Col>
             </Form.Row>
             <Form.Row>
               <InputGroup className="mb-3">
-                <Form.Control type="email" placeholder="E-Mail" required autoComplete="email"/>
+                <Form.Control type="email" placeholder="E-Mail" required autoComplete="email" name="email"
+                              isInvalid={validation.email} onChange={reset}/>
                 <InputGroup.Append>
                   <InputGroup.Text id="basic-addon1">
                     <FontAwesomeIcon icon={faAt}/>
                   </InputGroup.Text>
                 </InputGroup.Append>
+                <Form.Control.Feedback type="invalid">{validation.email && validation.email[0]}</Form.Control.Feedback>
               </InputGroup>
             </Form.Row>
             <Form.Row>
               <Col className="p-0 pr-2">
                 <InputGroup className="mb-1">
-                  <Form.Control type="password" placeholder="Password" required autoComplete="new-password"/>
+                  <Form.Control type="password" placeholder="Password" required autoComplete="new-password"
+                                name="password1" isInvalid={validation.password1} onChange={reset}/>
                   <InputGroup.Append>
                     <InputGroup.Text id="basic-addon1">
                       <FontAwesomeIcon icon={faKey}/>
                     </InputGroup.Text>
                   </InputGroup.Append>
+                  <Form.Control.Feedback type="invalid">{validation.password1 && validation.password1[0]}</Form.Control.Feedback>
                 </InputGroup>
               </Col>
               <Col className="p-0">
                 <InputGroup className="mb-1">
-                  <Form.Control type="password" placeholder="Repeat Password" required autoComplete="new-password"/>
+                  <Form.Control type="password" placeholder="Repeat Password" required autoComplete="new-password"
+                                name="password2" isInvalid={validation.password2} onChange={reset}/>
                   <InputGroup.Append>
                     <InputGroup.Text id="basic-addon1">
                       <FontAwesomeIcon icon={faKey} color="blue"/>
                     </InputGroup.Text>
                   </InputGroup.Append>
+                  <Form.Control.Feedback type="invalid">{validation.password2 && validation.password2[0]}</Form.Control.Feedback>
                 </InputGroup>
               </Col>
             </Form.Row>
+
+            {generic_error && <Alert variant="danger">{generic_error}</Alert>}
             <hr />
 
-            <Button type="submit" variant="primary" className="mr-1" onClick={onSignupClick}>Sign Up</Button>
+            <Button type="submit" variant="primary" className="mr-1">Sign Up</Button>
             <Button variant="secondary" onClick={onBack}>Back</Button>
 
           </form>
@@ -96,9 +146,7 @@ function SignUpSuccess({needApproval}) {
 
 export function SignUp({onSuccess, onBack}) {
   const [success, setSuccess] = useState(false);
-  const onSignupClick = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+  const onSignup = () => {
     setSuccess(true);
     setTimeout(() => {
       onBack();
@@ -106,6 +154,6 @@ export function SignUp({onSuccess, onBack}) {
   };
 
   return (
-      success? <SignUpSuccess/>: <SignUpDialog onBack={onBack} onSignupClick={onSignupClick} />
+      success? <SignUpSuccess/>: <SignUpDialog onBack={onBack} onSignup={onSignup} />
   );
 }
